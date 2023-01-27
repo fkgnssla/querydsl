41f0232a84b7c2ca190849aa458e475aa711f573
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-01-27 11:31:10 +0900

    Case 문

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -2,6 +2,8 @@ package study.querydsl.entity;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -393,6 +395,69 @@ public class QuerydslBasicTest {
             System.out.println("age = " +
                     tuple.get(select(memberSub.age.avg())
                             .from(memberSub)));
+        }
+    }
+
+    /**
+     * 단순한 조건
+     */
+    @Test
+    public void basicCase() {
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * 복잡한 조건
+     */
+    @Test
+    public void complexCase() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * 1. 0 ~ 30살이 아닌 회원 출력
+     * 2. 0 ~ 20살 회원 출력
+     * 3. 21 ~ 30살 회원 출력
+     */
+    @Test
+    public void orderByCase() {
+        NumberExpression<Integer> rankPath = new CaseBuilder()
+                .when(member.age.between(0, 20)).then(2) //0 ~ 20살 회원
+                .when(member.age.between(21, 30)).then(1) //21 ~ 30살 회원
+                .otherwise(3); //0 ~ 30살이 아닌 회원
+
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age, rankPath)
+                .from(member)
+                .orderBy(rankPath.desc()) //내림차순 정렬
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            Integer rank = tuple.get(rankPath);
+            System.out.println("username = " + username + " age = " + age + " rank = "
+                    + rank);
         }
     }
 }
