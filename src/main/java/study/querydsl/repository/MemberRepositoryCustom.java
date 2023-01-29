1c567ee91b85445105aa85ad9299fb2e6c303633
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-01-29 20:49:17 +0900

    스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -1,5 +1,7 @@
 package study.querydsl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 
@@ -8,4 +10,8 @@ import java.util.List;
 //사용자 정의 인터페이스 작성
 public interface MemberRepositoryCustom {
     List<MemberTeamDto> search(MemberSearchCondition condition);
+    //전체 카운트를 한번에 조회하는 단순한 방법
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    //데이터 내용과 전체 카운트를 별도로 조회하는 방법
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,7 +1,11 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -56,4 +60,65 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 
+    /**
+     * 전체 카운트를 한번에 조회하는 단순한 방법
+     */
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id, member.username, member.age,
+                        team.id, team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults(); //쿼리 2번을 날린다.(현재 쿼리, 카운트 쿼리)
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    /**
+     * 데이터 내용과 전체 카운트를 별도로 조회하는 방법
+     */
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id, member.username, member.age,
+                        team.id, team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch(); //현재 쿼리
+
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetchCount(); //카운트 쿼리
+
+        return new PageImpl<>(content, pageable, total);
+    }
 }
